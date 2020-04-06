5080496f2c16a8342059cbc30f2235761206a444
acubick <acubick@gmail.com>
2020-04-06 21:42:36 +0300

    Закончен модуль аутентификации и регистрации
    Took 27 minutes

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -22,7 +22,7 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
         password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
         name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
         agree: new FormControl(null, [Validators.requiredTrue])
@@ -41,5 +41,18 @@ export class RegistrationComponent implements OnInit {
           }
         })
       })
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any>{
+     return new Promise((resolve, reject) => {
+          this.usersService.getUserByEmail(control.value)
+            .subscribe((user: User) => {
+              if(user){
+                   resolve({forbiddenEmail: true})
+              } else {
+                resolve(null)
+              }
+            })
+     })
   }
 }
