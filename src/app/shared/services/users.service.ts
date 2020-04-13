21527ef6faa69063f271e6eba3658552b24c3250
acubick <acubick@gmail.com>
2020-04-13 19:37:20 +0300

    Закончено создание базового класса
    Took 45 minutes

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -2,31 +2,41 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable, OnInit } from '@angular/core'
 import { Observable } from 'rxjs'
 import { map, tap } from 'rxjs/operators'
+import { BaseApi } from '../core/base-api'
 import { User } from '../models/user.model'
 
 @Injectable({
   providedIn: 'root'
 })
   //? TODOFIX: Переделать регистрацию на FIREBASE
-export class UsersService implements OnInit{
+export class UsersService extends BaseApi{
 
   constructor(
-    private http: HttpClient
-  ) {}
-   getUserByEmail(email: string): Observable<User>{
+    public http: HttpClient
+  ) {
+    super(http)
+  }
+  //  getUserByEmail(email: string): Observable<User>{
+  //
+  //    return this.http.get<User>(`http://localhost:3000/users?email=${email}`)
+  //    //TODO: попробовать тут вернуть user[0] через метод map
+  //     .pipe( map((user: User) => user[0] ? user[0] : undefined))
+  //
+  // }
 
-     return this.http.get<User>(`http://localhost:3000/users?email=${email}`)
-     //TODO: попробовать тут вернуть user[0] через метод map
-      .pipe( map((user: User) => user[0] ? user[0] : undefined))
-
+  getUserByEmail(email: string): Observable<User>{
+    return this.get(`users?email=${email}`)
+      //TODO: попробовать тут вернуть user[0] через метод map
+               .pipe( map((user: User) => user[0] ? user[0] : undefined) )
   }
 
-  ngOnInit(): void {
-    console.log('this is user service')
-  }
+
+  // createNewUser(user: User): Observable<User>{
+  //    return this.http.post<User>(`http://localhost:3000/users`, user)
+  // }
+
   createNewUser(user: User): Observable<User>{
-     return this.http.post<User>(`http://localhost:3000/users`, user)
-
+    return this.post(`users`, user)
   }
 
 }
--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -2,20 +2,27 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { map, tap } from 'rxjs/operators'
+import { BaseApi } from '../../../shared/core/base-api'
 import { BillModel } from '../models/bill.model'
 
 @Injectable()
-export class BillService {
+export class BillService extends BaseApi{
   API_KEY           = 'b3723b8aec8f03cb656f1171ce12d3ed'
   base              = 'USD'
   symbols           = 'RUB,EUR,USD'
   currencyServerURL = `http://data.fixer.io/api/latest?access_key=${ this.API_KEY }`
 
-  constructor(private http: HttpClient) {}
+  constructor(public http: HttpClient) {
+    super(http)
+  }
+
+  // getBill(): Observable<BillModel> {
+  //   return this.http.get<BillModel>( 'http://localhost:3000/bill' )
+  //
+  // }
 
   getBill(): Observable<BillModel> {
-    return this.http.get<BillModel>( 'http://localhost:3000/bill' )
-
+    return this.get('bill')
   }
 
   getCurrency(base: string = 'USD'):Observable<any> {
