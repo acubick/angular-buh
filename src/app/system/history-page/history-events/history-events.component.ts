248217baf44997204d1c29b059ad3857b64295d4
acubick <acubick@gmail.com>
2020-04-17 22:14:55 +0300

    Закончен вывод списка под графиком
    Took 1 hour 16 minutes

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { Category } from '../../shared/models/category.model'
+import { APPEvent } from '../../shared/models/event.model'
 
 @Component({
   selector: 'app-history-events',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryEventsComponent implements OnInit {
 
+  @Input() categories: Category[] = []
+  @Input() events: APPEvent[]  = []
   constructor() { }
 
   ngOnInit(): void {
+    console.log('categories', this.categories)
+    console.log('events', this.events)
+    this.events.forEach((e) => {
+      //Ищем имя категории по id и возвращаем его
+      e.catName = this.categories.find(c => c.id === e.category).name
+    })
+
   }
 
+  getEventClass(e: APPEvent) {
+    //Определяем и возвращаем тип расход/доход
+       return {
+         'label': true,
+         'label-danger': e.type === 'outcome',
+         'label-success': e.type === 'income'
+       }
+  }
 }
